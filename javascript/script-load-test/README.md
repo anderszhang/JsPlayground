94bebb4c569afaa26990c3b3bd97067bcf3f4126
anderszhang@gmail.com <anderszhang@gmail.com>
2014-12-17 23:43:37 +0800

    add script load demo

---
+++ javascript/script-load-test/README.md
@@ -0,0 +1,6 @@
+append head
+异步方式加载,执行顺序为
+js1 appendChild Start
+js1 appendChild Finish
+js2 execute finish
+js1 execute finish
---
+++ javascript/script-load-test/js1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by snail on 12/17/14.
+ */
+for (var i = 0 ;i<100000000;i++){
+
+}
+console.log('js1 execute finish')
